a8a5847fb2abd14358b8581298bcebc33b75237b
FranciscoPalaciosA <francisco@nivelate.mx>
2019-09-04 18:13:21 -0500

    First queries

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,33 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (schema)
+
+const typeDefs = ` 
+    type Query {
+        hello: String!
+        name: String!
+    }
+
+`
+
+// Resolvers
+
+const resolvers = {
+    Query: {
+        hello() {
+            return "Hello World!"
+        },
+        name() {
+            return "Francisco Palacios"
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log("Server is up!")
+})
