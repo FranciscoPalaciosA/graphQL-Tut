81040baeb3e3b9dc98445eab01e3a172098035c4
FranciscoPalaciosA <francisco@nivelate.mx>
2019-09-05 19:21:31 -0500

    Update

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,6 +1,60 @@
 import { GraphQLServer } from 'graphql-yoga'
 
 // Scalar type: String, Boolean, Int, Float, ID
+
+// Demo user data
+const users = [{
+    id: '1',
+    name: 'Francisco Palacios',
+    email: 'francisco@example.com',
+    age: 22
+}, {
+    id: '2',
+    name: 'Alberto Martin',
+    email: 'betomarvel@example.com'
+}, {
+    id: '3',
+    name: 'Mauricio Siles',
+    email: 'msiles@example.com'
+}]
+
+const posts = [{
+    id: '11',
+    title: 'Título 1',
+    body: 'Body del primer post',
+    published: true,
+    author: '1'
+}, {
+    id: '12',
+    title: 'Título 2',
+    body: 'Body del segundo post',
+    published: false,
+    author: '1'
+}, {
+    id: '13',
+    title: 'Título 3',
+    body: 'Body del tercer post',
+    published: true,
+    author: '2'
+}]
+
+const comments = [{
+    id: '21',
+    text: 'Este es un comentario',
+    author: '1'
+}, {
+    id: '22',
+    text: 'Este es otro un comentario',
+    author: '1'
+}, {
+    id: '23',
+    text: 'Este también es un comentario',
+    author: '2'
+}, {
+    id: '24',
+    text: 'Este también es otro comentario',
+    author: '3'
+}]
  
 // Type definitions (schema)
 
@@ -8,7 +62,10 @@ const typeDefs = `
     type Query {
         me: User!
         post: Post!
-        users: [User!]!
+        comment: Comment!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments(query: String): [Comment!]!
     }
 
     type User {
@@ -16,6 +73,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
 
     type Post {
@@ -23,6 +81,13 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
     }
 `
 
@@ -46,8 +111,61 @@ const resolvers = {
                 published: false
             }
         },
+        comment(){
+            return {
+                id: 'Comment1',
+                text: 'Text of a comment'
+            }
+        },
         users(parent, args, ctx, info){
-            return 
+            if(!args.query){
+                return users
+            } 
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info){
+            if(!args.query){
+                return posts
+            }
+
+            return posts.filter((post) => {
+                return post.title.toLowerCase().includes(args.query.toLowerCase()) || 
+                post.body.toLowerCase().includes(args.query.toLowerCase()) 
+            })
+
+        },
+        comments(parent, args, ctx, info){
+            if(!args.query){
+                return comments
+            }
+
+            return comments.filter((comment) => {
+                return comment.text.toLowerCase().includes(args.query.toLowerCase())
+            })
+        }
+
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info){
+            return users.find((user) => {
+                return user.id === parent.author
+            })
         }
     }
 }
